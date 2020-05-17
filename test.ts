a2e608e33cb35e792e8ef4c6efa74fea0860fa13
oscar bernardini <bernardini687@gmail.com>
2020-05-17 11:33:50 +0200

    complete tests

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts'
-import * as minidate from './mod.ts'
+import miniDate from './mod.ts'
 
 // miniDate on the 5th of November 2020
 window.Date.now = () => Date.UTC(2020, 10, 5, 12)
@@ -11,6 +11,57 @@ Deno.test('Date.now() is overridden', (): void => {
 })
 
 Deno.test('current date is the 5th of November 2020', (): void => {
-  const subject = minidate.shape()
+  const subject = miniDate()
   assertEquals(`2020-11-05${TIME}`, subject.toISOString())
+})
+
+Deno.test('it accepts day as number', (): void => {
+  const subject = miniDate(1)
+  assertEquals(`2020-11-01${TIME}`, subject.toISOString())
+})
+
+Deno.test('it accepts DD', (): void => {
+  const subject = miniDate('01')
+  assertEquals(`2020-11-01${TIME}`, subject.toISOString())
+})
+
+Deno.test('it accepts D-M', (): void => {
+  const subject = miniDate('1-1')
+  assertEquals(`2020-01-01${TIME}`, subject.toISOString())
+})
+
+Deno.test('it accepts D-MM-YYYY', (): void => {
+  const subject = miniDate('1-01-2019')
+  assertEquals(`2019-01-01${TIME}`, subject.toISOString())
+})
+
+Deno.test('it accepts falsy values', (): void => {
+  let subject = miniDate()
+  assertEquals(`2020-11-05${TIME}`, subject.toISOString())
+
+  subject = miniDate('')
+  assertEquals(`2020-11-05${TIME}`, subject.toISOString())
+
+  subject = miniDate(0)
+  assertEquals(`2020-11-05${TIME}`, subject.toISOString())
+})
+
+Deno.test('it simply gives back `Invalid Date` on wrong inputs', (): void => {
+  let subject = miniDate(32)
+  assertEquals('Invalid Date', subject.toString())
+
+  subject = miniDate('2020')
+  assertEquals('Invalid Date', subject.toString())
+
+  subject = miniDate('2020-13-')
+  assertEquals('Invalid Date', subject.toString())
+
+  subject = miniDate('-13-')
+  assertEquals('Invalid Date', subject.toString())
+
+  subject = miniDate('-13-')
+  assertEquals('Invalid Date', subject.toString())
+
+  subject = miniDate('-')
+  assertEquals('Invalid Date', subject.toString())
 })
